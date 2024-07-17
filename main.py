b3a5c6907a13244d5b7912eef952d3b6ad877dd4
fiaiam <fiaiam@yandex.ru>
2024-07-17 19:24:23 +0300

    new

---
+++ main.py
@@ -0,0 +1,17 @@
+#1st program
+print(9 ** 0.5 * 5)
+
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+print(1234 % 1000 // 10 + 5678 % 1000 // 10)
+
+#4th program
+print(int(13.42) == 42.13 - int((42.13 * 100 % (int(42.13) * 100))) or int(42.13) == int((13.42 * 100 % (int(13.42) * 100))))
+
+print('Hi, PyCharm')
+x = 43
+y = 32
+print(x * y)
+print("End line")
